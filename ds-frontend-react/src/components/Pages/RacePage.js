b0df6e4ed0f19f2d74c51f5e029e16ad2e78853a
Brian Guo <38799617+IZenithI@users.noreply.github.com>
2020-12-06 03:35:42 -0500

    Implemented Line by Line Prompt

--- ds-frontend-react/src/components/Pages/RacePage.js
+++ ds-frontend-react/src/components/Pages/RacePage.js
@@ -10,16 +10,14 @@ class Racing extends Component {
         this.state = {
             text: '',
             userInput: '',
+            lastLine: '',
 
-            textArr: ["test1","test2","test3","test4","test5","test6"],
-            textArrLength: 30,
+            textString: "test1\ntest2\ntest3\ntest4\ntest5\n",
+            textArr: ["test1","test2","test3","test4","test5"],
+            textArrLength: 25,
             currentLine: 0,
-            lineCompleted: false,
-
 
             characters: 0,
-            correctInput: 0,
-            charLength: 0,
             seconds: 0,
             started: false,
             finished: false,
@@ -29,25 +27,34 @@ class Racing extends Component {
     }
 
     _handleKey(event){
-        if(event.key.length === 1)
-            console.log(event.key);
+        if(event.key.length === 1){}
+            //console.log(event.key);
     }
 
     setNextLine(event){
-        if(event.code === "Enter")
-            this.setState({currentLine: this.state.currentLine + 1})
-        
+        let lastLine = this.state.userInput.substr(this.state.userInput.indexOf("\n", this.state.userInput.length-this.state.textArr[this.state.currentLine].length-1)+1)
+        console.log("LastLine: " + lastLine + " Size: " + lastLine.length)
+        this.setState({lastLine: lastLine})
+        console.log("ArrayCurrentLine: " + this.state.textArr[this.state.currentLine] + " Size: " + lastLine.length)
+        if(event.code === "Enter" && lastLine !== this.state.textArr[this.state.currentLine]){
+            console.log("ENTERED")
+            if(this.state.currentLine < this.state.textArr.length-1){
+                this.setState({currentLine: this.state.currentLine + 1})
+            }
+        }
+
+        this.checkIfComplete()
     }
 
     componentDidMount(){
-        let prompt = "Commodo aute ipsum elit pariatur in officia magna esse exercitation laboris labore anim irure velit. Tempor eiusmod ut veniam id minim consequat. Dolor dolor anim sint ex non nulla officia magna ullamco est in. Amet sit quis fugiat adipisicing fugiat ullamco cillum exercitation."
-        this.setText(prompt)
-        this.setCharLength(prompt)
+        //let prompt = "Commodo aute ipsum elit pariatur in officia magna esse exercitation laboris labore anim irure velit. Tempor eiusmod ut veniam id minim consequat. Dolor dolor anim sint ex non nulla officia magna ullamco est in. Amet sit quis fugiat adipisicing fugiat ullamco cillum exercitation."
+        let prompt = this.state.textArr[this.state.currentLine]
+
         document.addEventListener("keyup", this._handleKey)
         document.addEventListener("keyup", this.setNextLine)
 
         this.props.socket.on("raceInit", ({text, error}) => {
-
+            
         })
 
         this.props.socket.on("updateText", ({playerName, percentage, placement}) => {
@@ -57,38 +64,34 @@ class Racing extends Component {
 
     componentWillUnmount(){
         document.removeEventListener("keyup", this._handleKey)
+        document.removeEventListener("keyup", this.setNextLine)
     }
 
-    setText=(prompt)=>{
-        this.setState({text: prompt})
-    }
-
-    setCharLength(prompt){
-        this.setState({charLength: prompt.replace(/\s+/g, '').length});
-    }
-
-    onUserInputChange=(e)=>{
-        const inputText = e.target.value;
-        this.setTimer();
-        this.onFinish(inputText);
-        this.setState({userInput: inputText, characters: this.countCorrectCharacters(inputText)})
-    }
-
-    onFinish(userInput) {
-        if(userInput === this.state.text) {
+    checkIfComplete(){
+        if(this.state.userInput === this.state.textString) {
+            console.log("Done")
+            document.getElementById("textArea").disabled = true;
             clearInterval(this.interval);
             this.setState({finished: true})
         }
     }
 
+    onUserInputChange=(e)=>{
+        const inputText = e.target.value;
+
+        this.setTimer();
+        this.setState({curUserInput: inputText,characters: this.countCorrectCharacters(inputText)})
+    }
+
     countCorrectCharacters(userInput){
-        const text = this.state.text.replace(/\s+/g, '');
-        let correctChars = userInput.replace(/\s+/g, '').split('').filter((s, i) => s === text[i]).length;
-        
-        // if(changed positively)
-        //     this.props.socket.emit('letterTyped', {lobbyCode: x, percentage: this.state.percentage})
-        
-        this.setState({percentage: correctChars/this.state.charLength})
+        const text = this.state.textString.replace(/\s+/g, '');
+        let correctChars = this.state.userInput.replace(/\s+/g, '').split('').filter((s, i) => s === text[i]).length;
+
+        //if(correctChars/this.state.textArrLength > this.state.percentage)
+            //this.props.socket.emit('letterTyped', {lobbyCode: x, percentage: this.state.percentage})
+        //console.log(this.state.correctInput)
+
+        this.setState({percentage: correctChars/this.state.textArrLength})
         return correctChars;
     }
 
@@ -103,49 +106,40 @@ class Racing extends Component {
         }
     }
 
-    onCorrectInput(input){        
-        return '#00FF00';
-    }
-
-    onIncorrectInput(){
-        return '#FF0000';
-    }
-
     render() {
         return (
             <div>
                 <h1>Vroom vroom racing</h1>
 
-                <Textblock text={this.state.text} userInput={this.state.userInput}/>
+                {/* <Textblock text={this.state.textArr[this.state.currentLine]} userInput={this.state.userInput}/> */}
 
-                {/* <div>
+                <div>
                     {
-                        this.state.text.split('').map((s,i) => {
+                        this.state.textArr[this.state.currentLine].split('').map((s,i) => {
                             let color;
-                            if (i < this.state.userInput.length){
-                                color = s === this.state.userInput[i] ? this.onCorrectInput() : this.onIncorrectInput();
+                            if (i < this.state.lastLine.length){
+                                color = s === this.state.lastLine[i] ? '#00FF00' : '#FF0000';
                             }
-                            return <span key={i} style={{color: color}}>{s}</span>
+                            return <span key={i} style={{backgroundColor: color}}>{s}</span>
                         })
                     }
-                </div> */}
+                </div>
+
+                <div>{this.state.textArr[this.state.currentLine+1]}</div>
+                <div>{this.state.textArr[this.state.currentLine+2]}</div>
 
                 <br></br>
 
                 <textarea
+                    id="textArea"
                     onChange={this.onUserInputChange}
                     value={this.state.userInput}
                     placeholder="Type Here"
                 ></textarea>
 
-                <div>
-                    {this.state.textArr[this.state.currentLine]} <br></br>
-                    {this.state.textArr[this.state.currentLine+1]} <br></br>
-                    {this.state.textArr[this.state.currentLine+2]} <br></br>
-                </div>
-
+                <div>{this.state.seconds}s</div>
                 <Speed seconds={this.state.seconds} characters={this.state.characters}/>
-
+                
                 <div>
                     {Math.round(this.state.percentage*100)}%
                 </div>
